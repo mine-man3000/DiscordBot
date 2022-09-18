53e13869107e5116872677a11e0348f3f39222bf
techflashYT <72118300+techflashYT@users.noreply.github.com>
2022-09-17 23:50:11 -0700

    Make the "ping" command reply to the message

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,9 @@
+const { Message } = require("discord.js");
+
 module.exports = {
-    name: 'ping',
-    description: "this is a command that tests to see if the bot works",
-    execute(message, args, Discord){
-        message.channel.send('pong!');
-    }
+	name: 'ping',
+	description: "this is a command that tests to see if the bot works",
+	execute(message, args, Discord) {
+		message.reply('Pong!');
+	}
 }
