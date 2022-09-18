aba7e24fa9e56bc681228b429c3956b987882fd5
techflashYT <72118300+techflashYT@users.noreply.github.com>
2022-09-18 01:20:34 -0700

    Fix a bunch of junk in unmute

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,29 +1,23 @@
 module.exports = {
-    name: 'unmute',
-    description: "this is an unmute",
-    execute(message, args, Discord){
-      const target = message.mentions.users.first();
-      const conf = require('../conf.json')
-        if(message.member.roles.cache.has(conf.modRoleID))
-        {
-          
-          if(target)
-          {
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');
+	name: 'unmute',
+	description: "this is an unmute",
+	execute(message, args, Discord) {
+		const conf = require('../conf.json')
+		if (!message.member.roles.cache.has(conf.modRoleID)) {
+			message.reply('You don\'t have permissions to use this command!');
+			return;
+		}
+		const target = message.mentions.users.first();
+		if (!target) {
+			message.reply('We couldn\'t find that member');
+			return;
+		}
+		let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');
 
-            let memberTarget = message.guild.members.cache.get(target.id);
+		let memberTarget = message.guild.members.cache.get(target.id);
 
-            memberTarget.roles.remove(muteRole.id)
-            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
-          }
-          else
-          {
-           message.channel.send('we couldn\'t find that member');
-          }
-        }
-        else
-        {
-          message.channel.send('You don\'t have permissions to use this commands');
-        }
-    }
+		memberTarget.roles.remove(muteRole.id)
+		message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+		// TODO: Log this in the log channel
+	}
 }
