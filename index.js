4cd9b98fc0a06fef7df5747c260e711fe419d5f1
techflashYT <72118300+techflashYT@users.noreply.github.com>
2022-09-20 16:22:50 -0700

    Make index.js better

--- index.js
+++ index.js
@@ -7,140 +7,127 @@ const conf = require('./conf.json');
 client.commands = new Discord.Collection();
  
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
  
-    client.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
 
 client.once('ready', () => {
-    console.log('Bot is online!');
-  });
+	console.log(`Bot is online!  User: ${client.username}`);
+});
 
+/*
+============== Member join ===============
+*/
 client.on('guildMemberAdd', guildMember =>{
-  console.log(guildMember.username + "joined");
+	console.log(guildMember.username + "joined");
 });
 
-
+/*
+============== Message edit ==============
+*/
 client.on('messageUpdate', (oldMessage, newMessage) => { // Old message may be undefined
-    if (!oldMessage.author) return;
-    const channel = client.channels.cache.get(conf.botLogChannelID)
-    var embed = new Discord.MessageEmbed()
-        .setAuthor("Message by " + newMessage.author.username + " updated in #" + oldMessage.channel.name)
-        .setTimestamp()
-        .setColor('#68ff61')
-        .addFields(
-            {name: 'original:',value: oldMessage},
-            {name: 'edit:', value: newMessage}    );
-        channel.send(embed);
-})
+	if (!oldMessage.author) {
+		return;
+	}
+	const channel = client.channels.cache.get(conf.botLogChannelID);
+	var embed = new Discord.MessageEmbed()
+		.setAuthor("Message by " + newMessage.author.username + " updated in #" + oldMessage.channel.name)
+		.setTimestamp()
+		.setColor('#68ff61')
+		.addField('Original:', oldMessage)
+		.addField('Edit:', newMessage);
 
-client.on('messageDelete', async message => {
-    const channel = client.channels.cache.get(conf.botLogChannelID)
-
-    const embed = new Discord.MessageEmbed()
-        .setTitle("Message Deleted")
-        .setDescription(message.content)
-        .addField('Author:', message.author.tag)
-        .addField('Channel:', message.channel.name)
-        .setTimestamp();
-        channel.send(embed)
+		channel.send(embed);
 });
+/*
+============= Message deleted ============
+*/
+client.on('messageDelete', async message => {
+	const channel = client.channels.cache.get(conf.botLogChannelID);
+
+	const embed = new Discord.MessageEmbed()
+		.setTitle("Message Deleted")
+		.setDescription(message.content)
+		.addField('Author:', message.author.tag)
+		.addField('Channel:', message.channel.name)
+		.setTimestamp();
+
+		channel.send(embed);
+});
+
+/*
+============== Message sent ==============
+*/
 
 client.on('message', message =>{
-    if(message.author.username == "mineman" && message.content == "uwu")
-      message.channel.send("uwu");
-    
-    if(message.content == ":3")
-        message.channel.send("colon three")
+	if (message.author.username == "mineman" && message.content == "uwu") {
+		message.reply("uwu");
+	}
+	
+	if (message.content == ":3") {
+		message.channel.send("colon three");
+	}
 
-    if(message.content == "4:")
-        message.channel.send("four colon")
+	if (message.content == "4:") {
+		message.channel.send("four colon");
+	}
 
-    if(message.content == "<@1009342267120685137>")
-        client.commands.get('help').execute(message, "", Discord);
+	/*
+	========== If pinged, send help ==========
+	*/
+	if (message.content == "<@1009342267120685137>") {
+		client.commands.get('help').execute(message, "", Discord);
+	}
 
-    let text = ""
+	let text = "";
 
-    if(message.content[1] == "~") return;
+	/*
+	=== If message is just "~", do nothing ===
+	*/
+	if (message.content[1] == "~") {
+		return;
+	}
 
-    const array = message.content.split(" ")
+	const nameArray = message.content.split(" ");
 
-    let value1 = Math.ceil(Math.random() * 100);
+	let someValue = Math.ceil(Math.random() * 100);
 
-    lowercase = message.content.toLowerCase();
+	lowercase = message.content.toLowerCase();
 
-    if(lowercase == "what's seshos" || lowercase == "what's seshos?" || lowercase == "whats seshos" || lowercase == "whats seshos?")
-    {
-        message.channel.send("SeshOS is the hobby operating system mineman is making\ngithub: <https://github.com/mine-man3000/SeshOS>\nchannel: <#1016214331609849896>\nbeware of GUI and Shell in the kernel...")
-    }
+	/*
+	If the message contains one of: "what's" OR "what is"
+	as well as "seshos", send a description of SeshOS
+	*/
+	if ((lowercase.contains("what\'s") || lowercase.contains("what is")) && lowercase.contains("seshos")) {
+		message.channel.send("SeshOS is the hobby operating system mineman is making\r\ngithub: <https://github.com/mine-man3000/SeshOS>\r\nchannel: <#1016214331609849896>\r\nbeware of GUI and Shell in the kernel...")
+	}
 
-    if(array[0] == "I'm" && !message.author.bot && value1 == 50) {
-      for (let i = 1; i < array.length; i++) {
-        text += array[i] + " ";
-      }
-      message.channel.send("Hi " + text + "I'm DisNBot")
-    }
+	if ((nameArray[0] == "I\'m" || nameArray[0] == "i\'m" || nameArray[0] == "Im" || nameArray[0] == "im") && !message.author.bot && someValue == 50) {
+		for (let i = 1; i < nameArray.length; i++) {
+			text += nameArray[i] + " ";
+		}
+		message.channel.send("Hi " + text + "I'm DisNBot");
+	}
 
 
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) {
+		return;
+	}
  
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
- 
-    switch(command) {
-      case "ping":
-          client.commands.get('ping').execute(message, args, Discord);
-          break;
-      
-      case "embedtest":
-          client.commands.get('embedtest').execute(message, args, Discord);
-          break;
-      
-      case "fact":
-          client.commands.get('fact').execute(message, args, Discord);
-          break;
-      
-      case "modtest":
-          client.commands.get('modtest').execute(message, args, Discord);
-          break;
-      
-      case "rickroll":
-          client.commands.get('rickroll').execute(message, args, Discord);
-          break;
-
-      case "ban":
-          client.commands.get('ban').execute(message, args, Discord, client);
-          break;
-      
-      case "kick":
-          client.commands.get('kick').execute(message, args, Discord, client);
-          break;
-
-      case "mute":
-          client.commands.get('mute').execute(message, args, Discord);
-          break;
-
-      case "unmute":
-          client.commands.get('unmute').execute(message, args, Discord);
-          break;
-
-      case "keysmash":
-          client.commands.get('keysmash').execute(message, args, Discord);
-          break;
-
-      case "help":
-          client.commands.get('help').execute(message, args, Discord);
-          break;
-
-      case "specs":
-          client.commands.get('specs').execute(message, args, Discord);
-          break;
-
-      default:
-          message.channel.send("error command \"" + command + "\" not found")
-    }
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	let command = client.commands.get(commandName);
+	
+	if (command != NULL) {
+		command.execute(message, args, Discord);
+	}
+	else {
+		message.channel.send(`Error: command \"${command}\" not found`);
+	}
+	
 });
 
-client.login(conf.token)
+client.login(conf.token);
