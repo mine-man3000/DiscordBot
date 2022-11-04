357d0cbe8403e2bd050bfc195b962d54edfd13d8
mine-man3000 <gdev0580@gmail.com>
2022-11-04 10:58:04 -0700

    made it log when people join and leave

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -9,5 +9,25 @@ module.exports = {
         }
         const channel = client.channels.cache.get(server);
         channel.send(`${member.user.tag} has left the server`)
+        const embed = {
+            color: 0x68ff61,
+            title: `${member.user.tag} has been left the server`,
+            author: {
+                name: `${member.user.tag}`,
+            },
+            fields: [
+                {
+                    name: "putting this here so I don't die",
+                    value: "putting this here so I don't die",
+                },
+            ]
+        }
+        for(i in config.guild) {
+            if(config.guild[i] == guild.id) {
+                server = config.botLogChannelID[i]
+            }
+        }
+        channel = client.channels.cache.get(server);
+        channel.send({ embeds: [embed] });
     }
 }
