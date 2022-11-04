861b7fcaf29327c51eecd04b34f6874b3431a068
mine-man3000 <gdev0580@gmail.com>
2022-11-04 11:05:10 -0700

    updated the help command

--- commands/help.js
+++ commands/help.js
@@ -30,6 +30,31 @@ module.exports = {
 					inline: false
 				},
 				{
+					name: 'throw:',
+					value: "throws an object at the given user",
+					inline: false
+				},
+				{
+					name: 'quote:',
+					value: "puts the given number of messages in a quote",
+					inline: false
+				},
+				{
+					name: 'role:',
+					value: "add/remove the given role",
+					inline: false
+				},
+				{
+					name: 'specs:',
+					value: "lists the specs of my machines",
+					inline: false
+				},
+				{
+					name: 'wiki:',
+					value: "info to add to the wiki",
+					inline: false
+				},
+				{
 					name: 'MOD COMMANDS:',
 					value: "\u200B",
 					inline: false
