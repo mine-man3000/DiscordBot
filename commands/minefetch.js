d7e817e1185227898721f7eae97242cd9fc72bad
mine-man3000 <gdev0580@gmail.com>
2023-07-22 23:37:18 -0700

    added more checks to minefetch

--- commands/minefetch.js
+++ commands/minefetch.js
@@ -5,11 +5,34 @@ module.exports = {
     description: "gets OS name and hostname of the system I'm running on",
     execute(ctx, Discord, client, conf) {
         if (fs.existsSync("/etc/os-release")) {
-            var lineReader = require('readline').createInterface({
+            var lineReader = ""
+	        var hostname = ""
+            fs.access("/etc/conf.d/hostname", fs.F_OK, (err) => {
+		        if(err) {
+		            fs.access("/etc/hostname", fs.F_OK, (err) => {
+		        		if(err) {
+		        		ctx.reply("running on a system that doesn't have ``/etc/hostname`` or ``/etc/conf.d/hostname``")
+		        		}
+		        	})
+		        	hostname = fs.readFileSync("/etc/hostname")
+                    
+                } else {
+		            console.log("hello?")
+                    lineReader = require('readline').createInterface({
+                        input: fs.createReadStream('/etc/conf.d/hostname')
+                    });
+
+                    lineReader.on('line', function (line) {
+		            	if (line.includes("hostname=")) {
+		            		hostname = line.split("=")[1].replace(/\"/g, '')
+                            console.log(line.split("=")[1].replace(/\"/g, ''))
+                        }
+		            })
+                }
+	        })
+            lineReader = require('readline').createInterface({
                 input: fs.createReadStream('/etc/os-release')
             });
-            
-            var hostname = fs.readFileSync("/etc/hostname", "utf8")
 
             lineReader.on('line', function (line) {
                 if (line.includes("PRETTY_NAME")) {
